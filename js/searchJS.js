d5f20e8854b286eeee23d28f23f22c0444ec96e4
wwmin <wwei.min@163.com>
2016-04-21 02:12:25 +0800

    条件查询
    ajax嵌套怎么用

---
+++ js/searchJS.js
@@ -0,0 +1,15 @@
+/**
+ * Created by wwm on 2016/4/20.
+ */
+new Vue({
+    el: '#searchDIV',
+    data: {
+        minValue:"0",
+        maxValue:"1"
+    },
+    method:{
+        search:function(){
+            console.log("进入搜索");
+        }
+    }
+});
